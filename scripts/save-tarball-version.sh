8b749e548f57897ad20fa1c9bca41208ae495dad
Andrew Kelley <andrew@ziglang.org>
2023-01-16 23:51:16 -0700

    delete unnecessary files

--- scripts/save-tarball-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "x${1}" == "x" ]; then
-    echo "Package version must be specified to generate tarball version"
-    exit 1
-fi
-
-echo "${1}" > "$MESON_DIST_ROOT/.tarball-version"
