3a1dfbe9ddfeded156dcf8f6b48694de45bb6d50
Andrew Kelley <andrew@ziglang.org>
2025-01-17 19:23:43 -0800

    bindings: add proplist

--- example/sine.zig
+++ example/sine.zig
@@ -4,8 +4,8 @@ pub fn main() !void {
     const main_loop = pa.threaded_mainloop.new() orelse return error.OutOfMemory;
     defer main_loop.free();
 
-    //const props = pa.proplist.new() orelse return error.OutOfMemory;
-    //defer props.free();
+    const props = pa.proplist.new() orelse return error.OutOfMemory;
+    defer props.free();
 
     //const context = pa.context.new_with_proplist(main_loop.get_api(), "sine example", props);
     //defer context.unref();
