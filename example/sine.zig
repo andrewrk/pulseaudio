3679ee59a1ee0a180b836e48bd282f4496c92844
Jonathan Marler <johnnymarler@gmail.com>
2025-08-07 14:39:06 -0600

    update sine example to generate sine wave

--- example/sine.zig
+++ example/sine.zig
@@ -5,6 +5,7 @@ const Pulse = struct {
     state: pa.context.state_t,
     stream_state: pa.stream.state_t,
     main_loop: *pa.threaded_mainloop,
+    sine_phase: f32,
 };
 
 pub fn main() !void {
@@ -29,6 +30,7 @@ pub fn main() !void {
         .state = .UNCONNECTED,
         .stream_state = .UNCONNECTED,
         .main_loop = main_loop,
+        .sine_phase = 0,
     };
     context.set_state_callback(contextStateCallback, &pulse);
 
@@ -91,7 +93,11 @@ pub fn main() !void {
 
         const fixed_sample_spec = stream.get_sample_spec();
         const fixed_channel_map = stream.get_channel_map();
-        std.log.info("fixed_sample_spec={any}", .{fixed_sample_spec});
+        std.log.info("fixed_sample_spec={}-channel {}Hz {s}", .{
+            fixed_sample_spec.channels,
+            fixed_sample_spec.rate,
+            @tagName(fixed_sample_spec.format),
+        });
         for (fixed_channel_map.map[0..fixed_channel_map.channels], 0..) |channel, i| {
             std.log.info("fixed_channel_map[{d}]={s}", .{ i, @tagName(channel) });
         }
@@ -132,18 +138,92 @@ fn streamStateCallback(stream: *pa.stream, userdata: ?*anyopaque) callconv(.c) v
 
 fn streamWriteCallback(stream: *pa.stream, requested_bytes: usize, userdata: ?*anyopaque) callconv(.c) void {
     const pulse: *Pulse = @ptrCast(@alignCast(userdata));
-    _ = pulse;
+    const sample_spec = stream.get_sample_spec();
     std.log.info("requested bytes: {d}", .{requested_bytes});
     var remaining_bytes = requested_bytes;
     while (remaining_bytes > 0) {
         var ptr_len: usize = remaining_bytes;
-        var opt_ptr: ?[*]i32 = null;
+        var opt_ptr: ?[*]u8 = null;
         stream.begin_write(@ptrCast(&opt_ptr), &ptr_len) catch @panic("unhandleable error");
         const ptr = opt_ptr orelse @panic("unhandleable error");
         const write_len = @min(ptr_len, remaining_bytes);
-        @memset(ptr[0..write_len], 0);
+        renderSine(&pulse.sine_phase, sample_spec.*, ptr, write_len);
         stream.write(ptr, write_len, null, 0, .RELATIVE) catch @panic("unhandleable error");
         remaining_bytes -= write_len;
         std.log.info("wrote {d} bytes, {d} remaining", .{ write_len, remaining_bytes });
     }
+}
+
+fn renderSine(sine_phase_ref: *f32, sample_spec: pa.sample_spec, buffer: [*]u8, len: usize) void {
+    const sample_len = sample_spec.format.sample_size();
+    const frame_len = sample_len * sample_spec.channels;
+    var offset: usize = 0;
+    while (offset + frame_len <= len) {
+        const sample = sampleFromF32(sample_spec.format, nextSineSample(sine_phase_ref, sample_spec.rate));
+        for (0..sample_spec.channels) |_| {
+            @memcpy((buffer + offset)[0..sample_len], sample.slice());
+            offset += sample_len;
+        }
+    }
+}
+
+fn nextSineSample(sine_phase_ref: *f32, sample_rate: u32) f32 {
+    const pitch: f32 = 440;
+    const volume: f32 = 0.3;
+
+    const phase_increment: f32 = 2.0 * std.math.pi * pitch / @as(f32, @floatFromInt(sample_rate));
+    const sample = volume * @sin(sine_phase_ref.*);
+    sine_phase_ref.* += phase_increment;
+    if (sine_phase_ref.* >= 2.0 * std.math.pi) {
+        sine_phase_ref.* -= 2.0 * std.math.pi;
+    }
+    return sample;
+}
+
+fn sampleFromF32(format: pa.sample_format_t, value: f32) std.BoundedArray(u8, 4) {
+    var result: std.BoundedArray(u8, 4) = undefined;
+    return switch (format) {
+        .S16LE => {
+            result.len = 2;
+            const scaled = @as(f32, @floatFromInt(std.math.maxInt(i16))) * value;
+            const sample = @as(i16, @intFromFloat(@round(scaled)));
+            std.mem.writeInt(i16, result.buffer[0..2], sample, .little);
+            return result;
+        },
+        .S16BE => {
+            result.len = 2;
+            const scaled = @as(f32, @floatFromInt(std.math.maxInt(i16))) * value;
+            const sample = @as(i16, @intFromFloat(@round(scaled)));
+            std.mem.writeInt(i16, result.buffer[0..2], sample, .big);
+            return result;
+        },
+        .FLOAT32LE => {
+            result.len = 4;
+            std.mem.writeInt(u32, result.buffer[0..4], @bitCast(value), .little);
+            return result;
+        },
+        .FLOAT32BE => {
+            result.len = 4;
+            std.mem.writeInt(u32, result.buffer[0..4], @bitCast(value), .big);
+            return result;
+        },
+        .S32LE => {
+            result.len = 4;
+            const scaled = @as(f64, @floatFromInt(std.math.maxInt(i32))) * @as(f64, value);
+            const sample = @as(i32, @intFromFloat(@round(scaled)));
+            std.mem.writeInt(i32, result.buffer[0..4], sample, .little);
+            return result;
+        },
+        .S32BE => {
+            result.len = 4;
+            const scaled = @as(f64, @floatFromInt(std.math.maxInt(i32))) * @as(f64, value);
+            const sample = @as(i32, @intFromFloat(@round(scaled)));
+            std.mem.writeInt(i32, result.buffer[0..4], sample, .big);
+            return result;
+        },
+        .U8, .S24LE, .S24BE, .S24_32LE, .S24_32BE, .ALAW, .ULAW => @panic(
+            "unsupported sample format",
+        ),
+        .MAX, .INVALID => unreachable,
+    };
 }
